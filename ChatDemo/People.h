eb2973401dc443989dad8ed79a04cbf97f322b31
wanghuaiyou <915325011@qq.com>
2016-09-13 19:58:55 +0800

    大改

---
+++ ChatDemo/People.h
@@ -0,0 +1,13 @@
+//
+//  People.h
+//  Demo
+//
+//  Created by wanghuaiyou on 16/9/13.
+//  Copyright © 2016年 wanghuaiyou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface People : NSObject
+
+@end
---
+++ ChatDemo/People.m
@@ -0,0 +1,13 @@
+//
+//  People.m
+//  Demo
+//
+//  Created by wanghuaiyou on 16/9/13.
+//  Copyright © 2016年 wanghuaiyou. All rights reserved.
+//
+
+#import "People.h"
+
+@implementation People
+
+@end
